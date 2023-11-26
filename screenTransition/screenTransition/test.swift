4dd5d6abb000e5f82f963b5daa1e821cf6d4af7f
halayumu <ookamiayumuwww@gmail.com>
2023-11-27 08:58:05 +0900

    削除ファイル

---
+++ screenTransition/screenTransition/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  screenTransition
+//
+//  Created by 石田歩夢 on 2023/11/11.
+//
+
+import Foundation
+import UIKit
+
+class test: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
